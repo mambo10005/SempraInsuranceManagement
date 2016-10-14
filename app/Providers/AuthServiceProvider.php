aaa87a13b191a8cfcb74bbe630f149e99eaa383b
Manho Joung <mjoung@sempra.com>
2016-10-14 09:08:54 -0700

    Initial Laravel Project

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SempraInsuranceManagement\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'SempraInsuranceManagement\Model' => 'SempraInsuranceManagement\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
